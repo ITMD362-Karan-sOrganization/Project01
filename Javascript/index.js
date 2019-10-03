91fa27f27abb75496ff1a275a0cf5d7bdcbc7314
Kpatel114 <31674536+Kpatel114@users.noreply.github.com>
2019-10-02 23:20:57 -0500

    fixed the two submit buttons

--- Javascript/index.js
+++ Javascript/index.js
@@ -1,4 +1,10 @@
-<button onclick= "submit()">Send me my free game </button>
+
+</script>function AlertScreen() {
+  alert("Thanks for joining the Sauer Community! You'll get your download link in your inbox soon!");
+
+  return false;
+  
+}<button onclick= "submit()">Send me my free game </button>
 
 <script>
 function submit() {
