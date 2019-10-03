c3b89561106d4d5c70f9368e06f91fb7ca9ecd72
Kpatel114 <31674536+Kpatel114@users.noreply.github.com>
2019-10-02 23:11:16 -0500

    added new javascript to html and js file.

--- Javascript/index.js
+++ Javascript/index.js
@@ -3,3 +3,10 @@ function AlertScreen() {
 
   return false;
 }
+<button onclick= "submit()">Send me my free game </button>
+
+<script>
+function submit() {
+  alert("Thanks for joining the Sauer Community! You'll get your download link in your inbox soon!");
+}
+</script>
