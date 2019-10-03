08145023ac711e3c22e1f55bf0ecbff30bdb9194
Kpatel114 <31674536+Kpatel114@users.noreply.github.com>
2019-10-02 23:14:20 -0500

    deleted inital alert function

---
+++ index.js
@@ -0,0 +1,7 @@
+<button onclick= "submit()">Send me my free game </button>
+
+<script>
+function submit() {
+  alert("Thanks for joining the Sauer Community! You'll get your download link in your inbox soon!");
+}
+</script>
