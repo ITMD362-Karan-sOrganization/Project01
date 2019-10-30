22f37d89c7534790e17c38c8696512cfe5575ff6
Kpatel114 <31674536+Kpatel114@users.noreply.github.com>
2019-10-30 14:32:49 -0500

    deleted uenused js fil

--- index.js
+++
@@ -1,7 +0,0 @@
-<button onclick= "submit()">Send me my free game </button>
-
-<script>
-function submit() {
-  alert("Thanks for joining the Sauer Community! You'll get your download link in your inbox soon!");
-}
-</script>
